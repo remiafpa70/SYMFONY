3a6bc8ad71e4e72812e0e07e06f472dba98e1335
remiafpa <remiafpa70@gmail.com>
2020-09-14 08:21:55 +0200

    1iere creation d'un projet symfony

---
+++ src/Form/ConducteurType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Conducteur;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ConducteurType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('prenom')
+            ->add('nom')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Conducteur::class,
+        ]);
+    }
+}
